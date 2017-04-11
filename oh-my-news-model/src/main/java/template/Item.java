de4985e7a74a1aa2180ff70390752ad99cfe89fb
songhanjian <hjsong1993@163.com>
2017-04-12 04:53:29 +0800

    模板修改 增加分页插件，增加时间控件

--- oh-my-news-model/src/main/java/template/Item.java
+++ oh-my-news-model/src/main/java/template/Item.java
@@ -40,20 +40,20 @@ public class Item {
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
