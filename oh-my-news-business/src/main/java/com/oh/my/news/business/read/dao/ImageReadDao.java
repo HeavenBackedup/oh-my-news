30afda2326bb76a8d78b16b3d2eb34a6c588d389
songhanjian <hjsong1993@163.com>
2017-05-09 05:56:48 +0800

    shj search bug 修复

--- oh-my-news-business/src/main/java/com/oh/my/news/business/read/dao/ImageReadDao.java
+++ oh-my-news-business/src/main/java/com/oh/my/news/business/read/dao/ImageReadDao.java
@@ -2,9 +2,12 @@ package com.oh.my.news.business.read.dao;
 
 import com.oh.my.news.model.po.File;
 
+import java.util.List;
+
 /**
  * Created by shj on 2017/4/13.
  */
 public interface ImageReadDao {
     File queryImageById(Integer id)throws Exception;
+    List<File> QueryImageByArticleId(Integer articleId)throws Exception;
 }
