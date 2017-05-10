a197760497ae5a9d113825bf4f4cd15e7e6e817b
duanzhengming1 <your_email@xxsjdw@foxmail.com>
2017-05-10 15:50:26 +0800

    段政名--我的钱包模块（私信Manage）

--- oh-my-news-web/src/main/java/com/oh/my/news/web/controller/ConcernAction.java
+++ oh-my-news-web/src/main/java/com/oh/my/news/web/controller/ConcernAction.java
@@ -1,54 +1,54 @@
-package com.oh.my.news.web.controller;
-
-import com.oh.my.news.business.read.manage.ConcernReadManage;
-import com.oh.my.news.model.dto.UserSnapshot;
-import com.oh.my.news.model.vo.myHomePage.home.OthersInfomation;
-import com.oh.my.news.web.util.BaseAction;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-/**
- * Created by fanfan on 2017/4/26.
- */
-@Controller
-@RequestMapping(value = "/concernPage")
-public class ConcernAction extends BaseAction{
-
-    @Resource
-    private ConcernReadManage concernReadManage;
-
-    @RequestMapping(value = "/getConcern")
-    public @ResponseBody
-    Object getFansInformation(@RequestBody Map userMap) {
-        //获取关注人列表
-        int userId = (Integer) userMap.get("userId");
-        try {
-            List<UserSnapshot> concerns = concernReadManage.getMyConcerns(userId);
-            List<OthersInfomation> concernList = new ArrayList<OthersInfomation>();
-            for (UserSnapshot u : concerns) {
-                OthersInfomation item = new OthersInfomation();
-                item.setUserId(u.getId());
-                item.setAvatarPath(u.getImageUrl());
-                item.setNickName(u.getNickname());
-                item.setSignature(u.getSignature());
-                concernList.add(item);
-            }
-            return successReturnObject(concernList);
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            return failReturnObject("get concerns fail");
-        }
-
-
-    }
-
-}
+//package com.oh.my.news.web.controller;
+//
+//import com.oh.my.news.business.read.manage.ConcernReadManage;
+//import com.oh.my.news.model.dto.UserSnapshot;
+//import com.oh.my.news.model.vo.myHomePage.home.OthersInfomation;
+//import com.oh.my.news.web.util.BaseAction;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import javax.annotation.Resource;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * Created by fanfan on 2017/4/26.
+// */
+//@Controller
+//@RequestMapping(value = "/concernPage")
+//public class ConcernAction extends BaseAction{
+//
+//    @Resource
+//    private ConcernReadManage concernReadManage;
+//
+//    @RequestMapping(value = "/getConcern")
+//    public @ResponseBody
+//    Object getFansInformation(@RequestBody Map userMap) {
+//        //获取关注人列表
+//        int userId = (Integer) userMap.get("userId");
+//        try {
+//            List<UserSnapshot> concerns = concernReadManage.getMyConcerns(userId);
+//            List<OthersInfomation> concernList = new ArrayList<OthersInfomation>();
+//            for (UserSnapshot u : concerns) {
+//                OthersInfomation item = new OthersInfomation();
+//                item.setUserId(u.getId());
+//                item.setAvatarPath(u.getImageUrl());
+//                item.setNickName(u.getNickname());
+//                item.setSignature(u.getSignature());
+//                concernList.add(item);
+//            }
+//            return successReturnObject(concernList);
+//
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return failReturnObject("get concerns fail");
+//        }
+//
+//
+//    }
+//
+//}
--- oh-my-news-web/src/main/java/com/oh/my/news/web/controller/FansAction.java
+++ oh-my-news-web/src/main/java/com/oh/my/news/web/controller/FansAction.java
@@ -1,53 +1,53 @@
-package com.oh.my.news.web.controller;
-
-import com.oh.my.news.business.read.manage.ConcernReadManage;
-import com.oh.my.news.model.dto.UserSnapshot;
-import com.oh.my.news.model.vo.myHomePage.home.OthersInfomation;
-import com.oh.my.news.web.util.BaseAction;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-/**
- * Created by fanfan on 2017/4/23.
- */
-
-@Controller
-@RequestMapping(value = "/fansPage")
-public class FansAction extends BaseAction{
-    @Resource
-    private ConcernReadManage concernReadManage;
-
-    @RequestMapping(value = "/getFans")
-    public @ResponseBody Object getFansInformation(@RequestBody Map userMap){
-        //获取粉丝列表
-        int userId = (Integer) userMap.get("userId");
-        try {
-            List<UserSnapshot> fans=concernReadManage.getMyFans(userId);
-            List<OthersInfomation> fansList=new ArrayList<OthersInfomation>();
-            for (UserSnapshot fan:fans){
-                OthersInfomation item=new OthersInfomation();
-                item.setUserId(fan.getId());
-                item.setAvatarPath(fan.getImageUrl());
-                item.setNickName(fan.getNickname());
-                item.setSignature(fan.getSignature());
-                fansList.add(item);
-            }
-            return successReturnObject(fansList);
-        }catch (Exception e){
-            e.printStackTrace();
-            return failReturnObject("get fans fail");
-        }
-
-
-    }
-
-
-}
+//package com.oh.my.news.web.controller;
+//
+//import com.oh.my.news.business.read.manage.ConcernReadManage;
+//import com.oh.my.news.model.dto.UserSnapshot;
+//import com.oh.my.news.model.vo.myHomePage.home.OthersInfomation;
+//import com.oh.my.news.web.util.BaseAction;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import javax.annotation.Resource;
+//import java.util.ArrayList;
+//import java.util.LinkedList;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * Created by fanfan on 2017/4/23.
+// */
+//
+//@Controller
+//@RequestMapping(value = "/fansPage")
+//public class FansAction extends BaseAction{
+//    @Resource
+//    private ConcernReadManage concernReadManage;
+//
+//    @RequestMapping(value = "/getFans")
+//    public @ResponseBody Object getFansInformation(@RequestBody Map userMap){
+//        //获取粉丝列表
+//        int userId = (Integer) userMap.get("userId");
+//        try {
+//            List<UserSnapshot> fans=concernReadManage.getMyFans(userId);
+//            List<OthersInfomation> fansList=new ArrayList<OthersInfomation>();
+//            for (UserSnapshot fan:fans){
+//                OthersInfomation item=new OthersInfomation();
+//                item.setUserId(fan.getId());
+//                item.setAvatarPath(fan.getImageUrl());
+//                item.setNickName(fan.getNickname());
+//                item.setSignature(fan.getSignature());
+//                fansList.add(item);
+//            }
+//            return successReturnObject(fansList);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            return failReturnObject("get fans fail");
+//        }
+//
+//
+//    }
+//
+//
+//}
